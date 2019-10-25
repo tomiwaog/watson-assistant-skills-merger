589196d423a138fca0959eb9b9fba991b487c8a9
Tomiwa Ogunbamowo <contact@tomiwa.io>
2019-10-25 18:51:41 +0100

    Added validation for skills violating intent limits Added validation for skills with Watson Entities upload limit

--- filesValidator.js
+++ filesValidator.js
@@ -1,48 +1,65 @@
 
-function validateFile(file){
+function validateFile(file) {
     var text = JSON.stringify(file);
     var isJson = (/^[\],:{}\s]*$/.test(text.replace(/\\["\\\/bfnrtu]/g, '@').
-    replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
-    replace(/(?:^|:|,)(?:\s*\[)+/g, '')));
+        replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').
+        replace(/(?:^|:|,)(?:\s*\[)+/g, '')));
     return isJson;
 }
 
 
-function validateWASkill(skill){
+function validateWASkill(skill) {
     //Known /common Watson Assistant Skill properties
-    validSkill ={
+    validSkill = {
         hasSkillId: skill.skill_id,
         hasWorkspaceID: skill["workspace_id"],
         hasIntent: skill["intents"]
     }
 
     //If any of the common properties are missing, then its not a valid Skill
-    for (eachPropery in validSkill){
+    for (eachPropery in validSkill) {
         if (!validSkill[eachPropery]) return false;
     }
 
     return true;
 }
 
-function validateBothFiles (file1, file2){
+function validateBothFiles(file1, file2) {
+    // console.log("skill1 intent count: " + file1.intents.length);
+    // console.log("skill2 intent count: " + file2.intents.length);
     var skill1 = file1, skill2 = file2;
     console.log("*** Running validator against files!")
-    try{
+    try {
         if (!validateFile(skill1))
             throw new TypeError('skill1 is not a valid JSON');
-        if (!validateFile(skill2))
+        if (skill2 && !validateFile(skill2))
             throw new TypeError('skill2 is not a valid JSON');
-    
+
         if (!validateWASkill(skill1))
-        throw new TypeError('skill1 is not a valid Watson Assistant Skill');
-        if (!validateWASkill(skill2))
+            throw new TypeError('skill1 is not a valid Watson Assistant Skill');
+        if (skill2 && !validateWASkill(skill2))
             throw new TypeError('skill2 is not a valid Watson Assistant Skill');
-    
-        console.log("*** Validation Check: No Exceptions violated!");
+
+        if (!isIntentsEntitiesExceeeded(skill1))
+            throw new TypeError('skill1 intents counts exceeds Watsons limit. Limit = 100, Maximum entities limit exceeded. Limit = 25.');
+
+        if (skill2 && !isIntentsEntitiesExceeeded(skill2))
+            throw new TypeError('skill2 intents counts exceeds Watsons limit. Limit = 100, Maximum entities limit exceeded. Limit = 25.');
+        
+            console.log("*** Validation Check: No Exceptions violated!");
+
     }
-    catch(error){
-            console.log("Error message: "+error.message);
+    catch (error) {
+        console.log("\nError message: " + error.message);
     }
 }
 
+function isIntentsEntitiesExceeeded(skill) {
+    var skill1IntentsCount = skill.intents.length, skill1EntitiesCount = skill.entities.length;
+    if (skill1IntentsCount > 100 || skill1EntitiesCount > 25) {
+        console.log("Warning!!! :"+skill.name+ " has intent count: "+ skill1IntentsCount+ " and entity count: "+skill1EntitiesCount);
+        return false;
+    }
+    return true;
+}
 module.exports = validateBothFiles;
