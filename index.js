f066bcee63f6f6b6706ffbe67b8afec59dc6a646
Tomiwa Ogunbamowo <contact@tomiwa.io>
2019-10-30 03:19:09 +0000

    Added version 0.1.5

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // var file1 = require('./sample-skills-files/skill-SkillOne'); //Location of your skill1
 // var file2 = require('./sample-skills-files/skill-SkillTwo'); //Location of your skill2
 
-
 var providedInterface = (file1, file2) => {
     require('./controller')(file1, file2);
 };
